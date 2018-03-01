28ca96ba0af5dfa86fc02387632b93664633420e
Jacob <jerling1989@gmail.com>
2018-02-28 21:40:25 -0500

    heroku connection typo

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$servername = "fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+	$servername = "fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 	$username = "rv97887ollhgdjl8";
 	$password = "tvg0et8i8jz7qfa7";
 	$dbname = "ix33e3fc1cscod83";
