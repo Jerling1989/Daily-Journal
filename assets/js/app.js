f083724989b2ec9cfdb4a6593de1c988e69abd6a
Jacob <jerling1989@gmail.com>
2018-03-01 01:50:40 -0500

    app.js comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
-
+// CREATE HEIGHT FOR TEXTAREA BASED ON USER SCREEN HEIGHT
 $('textarea').css('height', $(window).height() - 180);
 
+// WHENEVER THE USER ENTERS A KEY
 $('textarea').keyup(function() {
+	// RUN UPDATEJOURNAL.PHP SCRIPT TO UPDATE JOURNAL COLUMN WITH THE VALUE OF TEXTAREA
 	$.post('../php/updatejournal.php', {journal:$('textarea').val()})
 });
