7c09c76d5aa6baa33fa6ebe0aca758ebbae1e3ee
Jacob <jerling1989@gmail.com>
2018-03-01 01:37:10 -0500

    connection.php comments

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,10 +1,12 @@
 <?php
 
+	// CREATE VARIABLES FOR DATABASE CREDENTIALS
 	$servername = "fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 	$username = "rv97887ollhgdjl8";
 	$password = "tvg0et8i8jz7qfa7";
 	$dbname = "ix33e3fc1cscod83";
 
+	// CREATE LINK VARIABLE BY CONNECTING TO DATABASE
 	$link = mysqli_connect($servername, $username, $password, $dbname);
 
 ?>
