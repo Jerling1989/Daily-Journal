ee1161a8f8fa4fe25cf862df713b255584f8eafe
Jacob <jerling1989@gmail.com>
2018-03-01 00:52:27 -0500

    mainpage comments

--- assets/php/mainpage.php
+++ assets/php/mainpage.php
@@ -1,14 +1,18 @@
 <?php
-
+	
+	// START SESSION
 	session_start();
+	// INCLUDE CONNECTION.PHP SCRIPT
 	include('connection.php');
 
+	// GET JOURNAL ROW FROM USERS DB WHERE ID EQUALS USERS SESSION ID
 	$query = "SELECT journal FROM users WHERE id='".$_SESSION['id']."' LIMIT 1";
 
+	// GATHER RESULT FROM QUERY
 	$result = mysqli_query($link, $query);
-
+	// ENTER RESULT INTO ARRAY
 	$row = mysqli_fetch_array($result);
-
+	// DISPLAY RESULT FROM ARRAY
 	$journal = $row['journal'];
 
 ?>
@@ -46,36 +50,55 @@
 </head>
 
 <body id="main-page">
+
+	<!-- TOP NAV & LOGIN BAR -->
 	<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
 
+		<!-- APP LOGO -->
 	  <a class="navbar-brand" href="#"><span class="logo">Daily Journal</span> <i class="fas fa-pencil-alt"></i></a>
+
+	  <!-- MOBILE NAV COLLAPSE BUTTON -->
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
+	  <!-- END MOBILE NAV COLLAPSE BUTTON -->
 
+		<!-- LOGOUT DIV -->
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto"></ul>
 
+			<!-- LOGOUT FORM -->
 	    <form class="form-inline my-2 my-lg-0" method="post">
+	    	<!-- LOGOUT BUTTON -->
 				<a class="btn btn-outline-light" role="button" href="../../index.php?logout=1">
 					Log Out
 				</a>
 	    </form>
+	    <!-- END LOGOUT FORM -->
 
 	  </div>
+	  <!-- END LOGOUT DIV -->
+
 	</nav>
+	<!-- END TOP NAV & LOGIN BAR -->
+
 
 	<!-- JOURNAL ENTRY -->
 	<div class="container">
 		<div class="row">
 
+			<!-- SAVING DETAILS ALERT -->
 			<div id="disclaimer" class="alert alert-primary alert-dismissible fade show" role="alert">
+				<!-- SAVING INFORMATION -->
 			  <strong>Don't Worry About Losing Your Work!</strong> All of your updates and changes are saved automatically.
+			  <!-- ALERT CLOSE BUTTON -->
 			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
 			    <span aria-hidden="true">&times;</span>
 			  </button>
 			</div>
+			<!-- END SAVING DETAILS ALERT -->
 
+			<!-- JOURNAL TEXTAREA -->
 			<textarea class="form-control"><?php echo $journal; ?></textarea>
 		</div>
 	</div>
