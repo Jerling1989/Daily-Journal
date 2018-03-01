741bff1f29eb698d411f9df3d873d593e28b58e3
Jacob <jerling1989@gmail.com>
2018-02-28 21:29:20 -0500

    heroku db setup

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -1,5 +1,10 @@
 <?php
 
-	$link = mysqli_connect('localhost', 'root', 'root', 'journal_db');
+	$servername = "fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+	$username = "rv97887ollhgdjl8";
+	$password = "tvg0et8i8jz7qfa7";
+	$dbname = "ix33e3fc1cscod83";
+
+	$link = mysqli_connect($servername, $username, $password, $dbname);
 
 ?>
