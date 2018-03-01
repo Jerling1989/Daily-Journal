ce7693f20add8446dd2be07a2726c3e3a1d7095d
Jacob <jerling1989@gmail.com>
2018-03-01 01:46:54 -0500

    updatejournal.php comments

--- assets/php/updatejournal.php
+++ assets/php/updatejournal.php
@@ -1,12 +1,13 @@
 <?php
 
+	// START SESSION
 	session_start();
-
+	// INCLUDE CONNECTION.PHP SCRIPT
 	include('connection.php');
 
+	// QUERY TO UPDATE JOURNAL COLUMN WHERE USER ID IS EQUAL SESSION ID
 	$query = "UPDATE users SET journal='".mysqli_real_escape_string($link, $_POST['journal'])."' WHERE id='".$_SESSION['id']."' LIMIT 1";
-
+	// RUN QUERY IN DATABASE
 	mysqli_query($link, $query);
-
 
 ?>
