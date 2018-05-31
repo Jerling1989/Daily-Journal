ec5ba0b6d9db2083e47e399e5c39dc3dace24c82
Jacob <jerling1989@gmail.com>
2018-05-31 12:32:43 -0400

    remove break

--- index.php
+++ index.php
@@ -115,7 +115,6 @@
 	<br />
 	<br />
 	<br />
-	<br />
 
 <!-- JQUERY CDN -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
