d0ff0c06cc556fe077eca23a5706ce3ed90a973e
Jacob <jerling1989@gmail.com>
2018-05-31 12:30:52 -0400

    bottom margin

--- index.php
+++ index.php
@@ -112,7 +112,10 @@
 		</div>
 	</div>
 	<!-- END LOGIN PANEL -->
-
+	<br />
+	<br />
+	<br />
+	<br />
 
 <!-- JQUERY CDN -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
