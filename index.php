ca805b203fcb404c37baa6caeac4b6df6484cbef
Jacob <jerling1989@gmail.com>
2018-05-31 12:28:42 -0400

    update

--- index.php
+++ index.php
@@ -7,34 +7,25 @@
 
 	<!-- PAGE TITLE -->
 	<title>Daily Journal</title>
-
 	<!-- FAVICON -->
 	<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
-
 	<!-- META DATA -->
   <meta charset="utf-8" />
   <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-
 	<!-- FONT AWESOME -->
   <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
-
 	<!-- GOOGLE FONTS -->
   <link href="https://fonts.googleapis.com/css?family=Caveat+Brush|Passion+One|Roboto:500" rel="stylesheet">
-
 	<!-- RESET CSS LINK -->
   <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
-
 	<!-- CUSTOM CSS LINK -->
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	
 	<!-- BOOTSTRAP CDN -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
 </head>
 
 <body>
-
 	<!-- TOP NAV & LOGIN BAR -->
 	<nav id="login-nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
 
@@ -125,16 +116,12 @@
 
 <!-- JQUERY CDN -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-
 <!-- POPPER JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
 <!-- BOOTSTRAP JS -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 <!-- CUSTOM APP JS LINK -->
 <script type="text/javascript" src="assets/js/app.js"></script>
 
 </body>
 </html>
-
